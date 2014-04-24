76d0f1f4319e6fb83fb1e3e57f3b8c6d06cbd5c6
Kevin Phelps <KevinPhelps11@gmail.com>
2014-04-24 17:33:33 -0500

    added ExecuteCommand

--- IceAndFire/SQLiteDataUtility.cs
+++ IceAndFire/SQLiteDataUtility.cs
@@ -19,11 +19,22 @@ namespace IceAndFire
 				connection.Open();
 				using (SQLiteCommand command = new SQLiteCommand(sqlQuery, connection))
 				{
-					SQLiteDataReader reader =  command.ExecuteReader();
+					SQLiteDataReader reader = command.ExecuteReader();
 					DataTable table = new DataTable();
 					table.Load(reader);
 					return table;
 				}
+			}
+		}
+
+		public static void ExecuteCommand(string sqlCommand)
+		{
+			string connectionString = string.Format("Data Source={0};Version=3;", SQLiteDataUtility.DatabaseFilename);
+			using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+			{
+				connection.Open();
+				using (SQLiteCommand command = new SQLiteCommand(sqlCommand, connection))
+					command.ExecuteNonQuery();
 			}
 		}
 	}
